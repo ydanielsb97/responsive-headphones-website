1be39b8a751668d9204ad44c8e1eeb6724437fef
Yeison Serrano <ydanielsb97@gmail.com>
2023-12-01 06:59:52 -0400

    ✅ "About us" section ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,14 +21,18 @@ if(navClose){
 const navLink = document.querySelectorAll('.nav__link')
 
 const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*=============== ADD BLUR HEADER ===============*/
+const header = document.getElementById('header')
+const blurHeader = () =>{
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
 
 
 /*=============== SWIPER FAVORITES ===============*/ 
