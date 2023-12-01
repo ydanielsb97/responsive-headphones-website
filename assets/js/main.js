9da14bde8081113f2a481c2a8948e734a7c2c193
Yeison Serrano <ydanielsb97@gmail.com>
2023-12-01 07:14:13 -0400

    ✅ "Favorite" section ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,7 +36,18 @@ window.addEventListener('scroll', blurHeader)
 
 
 /*=============== SWIPER FAVORITES ===============*/ 
+let swiperFavorite = new Swiper('.favorite__swiper', {
+    loop: true,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
+    grabCursor: true,
 
+    breakpoints: {
+        768: {
+            slidesPerView: 3
+        }
+    }
+})
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
