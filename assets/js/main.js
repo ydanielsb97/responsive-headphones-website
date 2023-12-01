94b768dec86a1d28ff5d5092ff766138e2dd7749
Yeison Serrano <ydanielsb97@gmail.com>
2023-12-01 06:44:40 -0400

    ✅ Home section ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,6 +18,14 @@ if(navClose){
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*=============== ADD BLUR HEADER ===============*/
