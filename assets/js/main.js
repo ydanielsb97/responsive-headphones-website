144b18664c319caccccc13ece2dc541ae23da5dc
Yeison Serrano <ydanielsb97@gmail.com>
2023-12-01 08:06:43 -0400

    Scroll styles and scroll reveal implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,13 +26,44 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
-/*=============== ADD BLUR HEADER ===============*/
+/*=============== HANDLE SCROLL ===============*/
 const header = document.getElementById('header')
+const scrollUpBtn = document.getElementById('scroll-up')
+const sections = document.querySelectorAll('section[id]')
+
+const scrollUp = () =>{
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUpBtn.classList.add('show-scroll')
+						: scrollUpBtn.classList.remove('show-scroll')
+}
+
 const blurHeader = () =>{
     this.scrollY >= 50 ? header.classList.add('blur-header') 
                        : header.classList.remove('blur-header')
 }
-window.addEventListener('scroll', blurHeader)
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+const handlerScroll = () => {
+    blurHeader()
+    scrollUp()
+    scrollActive()
+}
+window.addEventListener('scroll', handlerScroll)
 
 
 /*=============== SWIPER FAVORITES ===============*/ 
@@ -49,10 +80,17 @@ let swiperFavorite = new Swiper('.favorite__swiper', {
     }
 })
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
-
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal('.home__social, .favorite__container, .sponsor__container, .footer')
+sr.reveal('.home__title span:nth-child(1)', { origin: 'left', opacity: 1})
+sr.reveal('.home__title span:nth-child(3)', { origin: 'right', opacity: 1})
+sr.reveal('.home__tooltip, .home__button, .model__button', {origin: 'bottom'})
+sr.reveal('.about__data', {origin: 'left'})
+sr.reveal('.about__img, .model__tooltip', {origin: 'right'})
